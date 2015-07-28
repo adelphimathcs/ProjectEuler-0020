1474a2962f24734648214c7672396f5e0d8f398e
Michael Vessia <michaelvessia@mail.adelphi.edu>
2015-07-28 10:54:34 -0400

    Update PE0020.rb

--- Ruby/PE0020.rb
+++ Ruby/PE0020.rb
@@ -1,15 +1,12 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
-
-def sum_digits(n)
-  return n==0 ? 0 : n%10 + sum_digits(n/10)
-end
-
-# http://rosettacode.org/wiki/Factorial#Ruby
 class Integer
   def fact
     (1..self).reduce(:*) || 1
   end
+  
+  def sum_digits
+  return self==0 ? 0 : self%10 + (self/10).sum_digits
+end
+  
 end
 
-puts sum_digits(100.fact)
+puts 100.fact.sum_digits
